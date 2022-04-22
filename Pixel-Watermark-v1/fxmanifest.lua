065859f659b7f532c6d732bedf381d9a9cddca66
Pixel <100500099+PixelTheDeveloper@users.noreply.github.com>
2022-04-22 23:48:46 +0200

    Update fxmanifest.lua

--- Pixel-Watermark-v1/fxmanifest.lua
+++ Pixel-Watermark-v1/fxmanifest.lua
@@ -1,22 +1,25 @@
-fx_version 'cerulean'
-games { 'gta5' }
-
+fx_version 'adamant'
+game 'gta5'
 author 'Pixel#7068'
-description 'Pixel Watermark'
-version '1.0.0
-
-ui_page('html/index.html')
-client_scripts {
-    'client.lua',
-}
+description 'Pixel Watermark v1'
+version '1.0.0'
 
 server_scripts {
-    'server.lua',
+	'server.lua'
 }
-files({
+
+client_scripts {
+	'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
     'html/index.html',
     'html/css/style.css',
     'html/css/bootstrap.min.css',
     'html/js/script.js',
     'html/js/bootstrap.min.js'
-})
+}
+
+lua54 'yes'
